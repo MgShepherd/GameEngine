27c7c2f8e65a66501415350a193013ebf4e7c517
MgShepherd <michaelshep1504@gmail.com>
2025-06-08 21:20:52 +0100

    Added clang format file and formatted main accordingly

--- src/main.c
+++ src/main.c
@@ -3,25 +3,25 @@
 #include <GLFW/glfw3.h>
 
 int main() {
-	if (glfwInit() == GLFW_FALSE) {
-		printf("Unable to initialise GLFW\n");
-		return -1;
-	}
+  if (glfwInit() == GLFW_FALSE) {
+    printf("Unable to initialise GLFW\n");
+    return -1;
+  }
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Game Engine", NULL, NULL);
-	if (window == NULL) {
-		printf("Unable to create GLFW window\n");
-		return -1;
-	}
+  GLFWwindow *window = glfwCreateWindow(640, 480, "Game Engine", NULL, NULL);
+  if (window == NULL) {
+    printf("Unable to create GLFW window\n");
+    return -1;
+  }
 
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-	}
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
 
-	printf("Successfully initialised GLFW and created window\n");
+  printf("Successfully initialised GLFW and created window\n");
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
+  glfwDestroyWindow(window);
+  glfwTerminate();
 
-	return 0;
+  return 0;
 }
